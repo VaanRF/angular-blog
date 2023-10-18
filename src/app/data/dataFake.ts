da77f59d234ae6bd69d6dff41f2d0e8f50d1ba54
VaanRF <vanderson.ribeirof@gmail.comm>
2023-10-18 18:39:20 -0300

    first commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":1,
+        "photoCover":"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png",
+        "title":"Titulo dinamico 1",
+        "description":"Descrição dinamica 1"
+    },
+    {
+        "id":2,
+        "photoCover":"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png",
+        "title":"Titulo dinamico 2",
+        "description":"Descrição dinamica 2"
+    },
+    {
+        "id":3,
+        "photoCover":"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png ",
+        "title":"Titulo dinamico 3",
+        "description":"Descrição dinamica 3"
+    },
+    {
+        "id":4,
+        "photoCover":"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/310px-Placeholder_view_vector.svg.png",
+        "title":"Titulo dinamico 4",
+        "description":"Descrição dinamica 4"
+    }
+]
